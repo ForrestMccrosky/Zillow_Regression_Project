26b0e9c36b3ac7f1ca13018cd2fa7713a3c90661
Your Name <forrest.mccrosky@gmail.com>
2021-06-11 17:03:16 -0500

    adding part of acquire data pipeline process

---
+++ zillow_acquire.py
@@ -0,0 +1,55 @@
+#############################Function File for Data Acquire############################
+
+
+
+from math import sqrt
+from scipy import stats
+from pydataset import data
+from datetime import datetime
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+import sklearn.metrics
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from env import host, user, password, sql_connect
+
+
+
+def wrangle_zillow():
+    '''
+    This function is going to acquire the neccessary columns bedroomcnt, bathroomcnt, 
+    calculatedfinishedsquarefeet, taxvaluedollorcnt, yearbuilt, taxamount, and fips         from
+    the zillow databse in SQL and move it into a pandas dataframe while
+    filtering for Single Family Residential properties
+    
+    The function will then clean the null values by dropping them because the               percentage
+    of rows with null values was very small compared to the 2.15 million rows of           the dataframe
+    '''
+    ## sql query to select the neccessary columns needed for project
+    
+    ## while using where to filter for homes with the Id = 261 because those
+    ## are single family homes
+    sql_query = '''
+    select bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt,       yearbuilt, taxamount, fips
+    from properties_2017
+    where propertylandusetypeid in (260, 261, 262, 263, 264, 265, 266, 273, 275, 276,       279); 
+    '''
+    
+    ## Connect to the Zilllw database
+    df = pd.read_sql(sql_query, sql_connect('zillow'))
+    
+    ## We are going to drop the null rows because of the 
+    df = df.dropna()
+    
+    df = df.reset_index()
+    
+    df = df.drop(columns = 'index')
+    
+    return df
+
+
+
